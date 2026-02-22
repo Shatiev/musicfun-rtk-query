ba335540efbc4078197ca580eba90985ac3bcdac
Shatiev <mr_shatiev@mail.ru~>
2026-02-23 03:35:11 +0400

    rtk query

---
+++ src/playlistsApi.ts
@@ -0,0 +1,24 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import type {PlaylistsResponse} from "@/playlistsApi.types.ts";
+
+export const playlistsApi = createApi({
+    reducerPath: 'playlistsApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: import.meta.env.VITE_BASE_URL,
+        headers: {
+            'API-KEY': import.meta.env.VITE_API_KEY,
+        },
+    }),
+    endpoints: build => ({
+        fetchPlaylists: build.query<PlaylistsResponse, void>({
+            query: () => {
+                return {
+                    method: 'get',
+                    url: `playlists`,
+                }
+            },
+        }),
+    }),
+})
+
+export const { useFetchPlaylistsQuery } = playlistsApi
